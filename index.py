005fa1ee7f837edc7c98b250a34dd45f89324b1b
Zachary Boelter <zboelter@gmail.com>
2023-03-29 17:46:18 -0700

    exercise 3

--- index.py
+++ index.py
@@ -5,3 +5,26 @@ print('This is fun')
 print('Yay! Printing')
 print('Id much rather you "not"')
 print("i said 'do not' touch this")
+
+print('I will now count my chickens')
+
+print("Hens", 25 + 30 /6)
+print("roosters", 100 - 25 * 3 % 4)
+
+print("Now i will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is that true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("what is 5 - 7?", 5 - 7)
+
+print("Oh that's why it's false.")
+
+print("How about some more?")
+print("Is it greater?", 5 > -2)
+print("Is it great or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
