82d6e2a0e8c4d28ac674540cc91875daa9dda98a
Zachary Boelter <zboelter@gmail.com>
2023-03-29 17:58:12 -0700

    Exercise 4

--- index.py
+++ index.py
@@ -1,30 +1,47 @@
-print('Hello, World!')
-print('Hello again')
-print('I like typing this.')
-print('This is fun')
-print('Yay! Printing')
-print('Id much rather you "not"')
-print("i said 'do not' touch this")
+# print('Hello, World!')
+# print('Hello again')
+# print('I like typing this.')
+# print('This is fun')
+# print('Yay! Printing')
+# print('Id much rather you "not"')
+# print("i said 'do not' touch this")
 
-print('I will now count my chickens')
+# print('I will now count my chickens')
 
-print("Hens", 25 + 30 /6)
-print("roosters", 100 - 25 * 3 % 4)
+# print("Hens", 25 + 30 /6)
+# print("roosters", 100 - 25 * 3 % 4)
 
-print("Now i will count the eggs:")
+# print("Now i will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
-print("Is that true that 3 + 2 < 5 - 7?")
+# print("Is that true that 3 + 2 < 5 - 7?")
 
-print(3 + 2 < 5 - 7)
+# print(3 + 2 < 5 - 7)
 
-print("What is 3 + 2?", 3 + 2)
-print("what is 5 - 7?", 5 - 7)
+# print("What is 3 + 2?", 3 + 2)
+# print("what is 5 - 7?", 5 - 7)
 
-print("Oh that's why it's false.")
+# print("Oh that's why it's false.")
 
-print("How about some more?")
-print("Is it greater?", 5 > -2)
-print("Is it great or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+# print("How about some more?")
+# print("Is it greater?", 5 > -2)
+# print("Is it great or equal?", 5 >= -2)
+# print("Is it less or equal?", 5 <= -2)
+
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 30
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("there are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
