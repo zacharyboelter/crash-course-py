0f6655b531412c916f5b631de6719514a30b117a
Zachary Boelter <zboelter@gmail.com>
2023-04-02 13:21:07 -0700

    concat with strings and .format()

--- index.py
+++ index.py
@@ -97,3 +97,19 @@ e = 'that has a right side.'
 print('Mary had a little lamb.')
 print('Its fleece as white as {}'.format('snow'))
 print('.' * 10)
+
+end1 = 'C'
+end2 = 'H'
+end3 = 'E'
+end4 = 'E'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end = '')
+print(end7 + end8 + end9 + end10 + end11 + end12)
